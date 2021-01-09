c3ff4750a9d0221a689fe3ef8bdf465f58ffccd1
roypulak <roy1pulak@gmail.com>
2021-01-09 12:16:38 +0100

    [9] handle a delete request

--- index.js
+++ index.js
@@ -65,6 +65,19 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found.');
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
 function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
