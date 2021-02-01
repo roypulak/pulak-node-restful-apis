a15bb83ef07185c54670726b8a9cc86b8b30fb82
roypulak <roy1pulak@gmail.com>
2021-02-01 17:43:49 +0100

    [31.11] exercise 2

---
+++ mongo-demo/exercises/exercise-2-solution.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+//ingeneral the connection url should be feeded from configuration for security concern
+mongoose.connect('mongodb://localhost/mongo-exercises')
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.error('Could not connect to MongoDB..', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date },
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+    return await Course
+        .find({ isPublished: true })
+        .or([ {tags: 'frontend'}, {tags: 'backend'}])
+        .sort({ price: -1 })
+        .select({ name: 1, author: 1, price: 1 });
+}
+
+async function searchCourses() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+searchCourses();
