8a60d83737eb84da2608e00ad97642c0d4f79599
roypulak <roy1pulak@gmail.com>
2021-02-15 15:16:39 +0100

    [33.06] project - build the movies api

--- vidly.js
+++ vidly.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose')
 const Joi = require('joi');
 const genres = require('./routes/vidly/genres');
 const customers = require('./routes/vidly/customers');
+const movies = require('./routes/vidly/movies');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use('/api/vidly/genres', genres);
 app.use('/api/vidly/customers', customers);
+app.use('/api/vidly/movies', movies);
 
 mongoose.connect('mongodb://localhost/vidly')
-.then(() => console.log('Connected to MongoDB..'))
-.catch(err => console.error('Could not connect to MongoDB..', err))
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.error('Could not connect to MongoDB..', err))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
