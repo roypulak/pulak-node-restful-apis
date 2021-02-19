7b1915f21c8ba74cad296767b6b819a8ff3b7628
roypulak <roy1pulak@gmail.com>
2021-02-19 10:18:05 +0100

    [33.07] Project- Build the Rentals API

--- vidly.js
+++ vidly.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const genres = require('./routes/vidly/genres');
 const customers = require('./routes/vidly/customers');
 const movies = require('./routes/vidly/movies');
+const rentals = require('./routes/vidly/rentals');
 const express = require('express');
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api/vidly/genres', genres);
 app.use('/api/vidly/customers', customers);
 app.use('/api/vidly/movies', movies);
+app.use('/api/vidly/rentals', rentals);
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB..'))
