cb735f40915f2ed8b5b6f1873f1dfc6c9d5107ff
roypulak <roy1pulak@gmail.com>
2021-02-22 16:47:27 +0100

    [33.09] objectID

---
+++ objectid.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const id = mongoose.Types.ObjectId();
+console.log(id.getTimestamp());
+
+const isValid = mongoose.Types.ObjectId.isValid('1234');
+
+console.log(isValid);
