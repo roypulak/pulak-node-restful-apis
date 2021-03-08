d7f8eb1f49c173b1a36918b73ee4f602438c29e6
roypulak <roy1pulak@gmail.com>
2021-03-08 17:46:33 +0100

    [34.06] authenticating users

--- vidly.js
+++ vidly.js
@@ -6,6 +6,7 @@ const customers = require('./routes/vidly/customers');
 const movies = require('./routes/vidly/movies');
 const rentals = require('./routes/vidly/rentals');
 const users = require('./routes/vidly/users');
+const auth = require('./routes/vidly/auth');
 const express = require('express');
 const app = express();
 
@@ -15,6 +16,7 @@ app.use('/api/vidly/customers', customers);
 app.use('/api/vidly/movies', movies);
 app.use('/api/vidly/rentals', rentals);
 app.use('/api/vidly/users', users);
+app.use('/api/vidly/auth', auth);
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB..'))
