a45f144f44812147e24f928ec39fa4a376afc0f6
roypulak <roy1pulak@gmail.com>
2021-03-12 15:13:04 +0100

    [34.14] protecting routes

--- routes/vidly/genres.js
+++ routes/vidly/genres.js
@@ -1,3 +1,4 @@
+const authMiddleware = require('../../middleware/auth');
 const { Genre, validate } = require('../../models/genre');
 const express = require('express');
 const router = express.Router();
@@ -7,8 +8,7 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
-  console.log(req.body);
+router.post('/', authMiddleware, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
