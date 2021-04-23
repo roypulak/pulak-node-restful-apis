07f73a3b97437ebfb385d51e0dfb69e37dc7d3c3
roypulak <roy1pulak@gmail.com>
2021-04-23 08:48:26 +0200

    [35.11] extracting routes

--- vidly.js
+++ vidly.js
@@ -1,31 +1,25 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const errorMiddlewire = require('./middleware/error');
 const dotenv = require('dotenv');
 const config = require('config');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/vidly/genres');
-const customers = require('./routes/vidly/customers');
-const movies = require('./routes/vidly/movies');
-const rentals = require('./routes/vidly/rentals');
-const users = require('./routes/vidly/users');
-const auth = require('./routes/vidly/auth');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
 
 winston.handleExceptions(
-    new winston.transports.File({filename: 'uncauchtException.log'}));
+    new winston.transports.File({ filename: 'uncauchtException.log' }));
 
 process.on('unhandledRejection', (ex) => {
     throw ex;
 });
 
-winston.add(new winston.transports.File({ filename: 'logfile.log'}));
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 winston.add(new winston.transports.MongoDB(
-    { 
+    {
         db: 'mongodb://localhost/vidly',
         level: 'error'
     }
@@ -40,14 +34,6 @@ if (!process.env.VIDLY_JWT_PRIVATE_KEY) {
     process.exit(1);
 }
 
-app.use(express.json());
-app.use('/api/vidly/genres', genres);
-app.use('/api/vidly/customers', customers);
-app.use('/api/vidly/movies', movies);
-app.use('/api/vidly/rentals', rentals);
-app.use('/api/vidly/users', users);
-app.use('/api/vidly/auth', auth);
-app.use(errorMiddlewire);
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB..'))
