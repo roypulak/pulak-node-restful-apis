8a34d1f98847a9dd0b99182d0190555147428f9f
roypulak <roy1pulak@gmail.com>
2021-04-30 09:18:20 +0200

    [35.13] extracting the logging logic

--- vidly.js
+++ vidly.js
@@ -1,32 +1,13 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const dotenv = require('dotenv');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
+
+require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
-
-winston.handleExceptions(
-    new winston.transports.File({ filename: 'uncauchtException.log' }));
-
-process.on('unhandledRejection', (ex) => {
-    throw ex;
-});
-
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB(
-    {
-        db: 'mongodb://localhost/vidly',
-        level: 'error'
-    }
-));
-
-const p = Promise.reject(new Error('Something failed miserably!'));
-p.then(() => console.log('Done'));
 
 dotenv.config();
 if (!process.env.VIDLY_JWT_PRIVATE_KEY) {
